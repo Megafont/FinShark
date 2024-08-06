d4dd81846dced8e28253082e4f1137c851094aa6
Megafont <51346737+Megafont@users.noreply.github.com>
2024-08-06 15:19:16 -0500

    Finished FrontEnd Video #5
    + Added a search box

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import CardList from './Components/CardList/CardList';
+import Search from './Search/Search';
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <CardList />
     </div>
   );
