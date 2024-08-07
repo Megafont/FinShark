f0a45a5ec86421eec11a424ccc3c623c60199b5b
Megafont <51346737+Megafont@users.noreply.github.com>
2024-08-06 19:53:38 -0500

    Finished FrontEnd Video #9
    + Search box works, but output can only be seen in browser console for now

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -2,20 +2,38 @@ import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Search/Search';
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 
-function App() {
+function App()
+{
   const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>("");
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
+  {
     setSearch(e.target.value);
     console.log(e);
   };
 
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
-  };
+  const onClick = async (e: SyntheticEvent) =>
+  {
+    const result = await searchCompanies(search);
 
+    // Do type narrowing
+    if (typeof result === "string")
+    {
+      setServerError(result);
+    }
+    else if (Array.isArray(result.data))
+    {
+      setSearchResult(result.data);
+    }
+
+    console.log(searchResult);
+  };
 
   return (
     <div className="App">
@@ -23,6 +41,7 @@ function App() {
       <CardList />
     </div>
   );
+
 }
 
 export default App;
